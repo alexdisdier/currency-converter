b3f6764e84644da72c76a72683da3f70b900015d
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-02-07 14:28:31 +0100

    Updates

---
+++ precache-manifest.459707799ebd31671fb7abe1e9c8bf80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dcc8f8cd605134658ce",
+    "url": "/currency-converter/static/js/runtime~main.1dcc8f8c.js"
+  },
+  {
+    "revision": "c452bf048b8cd0049221",
+    "url": "/currency-converter/static/js/main.c452bf04.chunk.js"
+  },
+  {
+    "revision": "633623ffd4519348599d",
+    "url": "/currency-converter/static/js/1.633623ff.chunk.js"
+  },
+  {
+    "revision": "c452bf048b8cd0049221",
+    "url": "/currency-converter/static/css/main.c7482f08.chunk.css"
+  },
+  {
+    "revision": "c4e56626e8e8ba5d13ec8f28c13ee2a9",
+    "url": "/currency-converter/index.html"
+  }
+];
